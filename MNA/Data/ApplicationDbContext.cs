4f72119e9a2b09d6925fffa3111022145cde0728
ahmed Elbelkasy <nanom4356@gmail.com>
2025-01-13 21:39:13 +0200

    Add project files.

---
+++ MNA/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MNA.Models;
+namespace MNA.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    } 
+}
